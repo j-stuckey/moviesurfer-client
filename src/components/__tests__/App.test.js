19ae2df1ef6112cc4047f39ffd95a447c4d3dad0
James Stuckey <jstuckey@rutgers.edu>
2019-03-15 12:59:04 -0400

    added describe block to App test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,9 +4,11 @@ import App from '../App';
 
 import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
+describe("<App />", () =>{
 
-	const wrapper = shallow(<App />);
-
-	expect(wrapper.exists()).toBe(true);
-});
+	it('renders without crashing', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.exists()).toBe(true);
+	});
+	
+})
