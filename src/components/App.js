876b5db4c9762274e2579cd5f4671dac70aa0895
James Stuckey <jstuckey@rutgers.edu>
2019-03-15 13:27:33 -0400

    removed base App style

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './styles/App.css';
 
 class App extends Component {
     render() {
-        return <div>
-			<h1>Welcome to React</h1>
-		</div>;
+        return (
+            <div>
+                <h1>Welcome to React</h1>
+            </div>
+        );
     }
 }
 
