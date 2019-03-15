209ae4a94782ef9250a6800e86a5774b516cd377
James Stuckey <jstuckey@rutgers.edu>
2019-03-15 12:33:55 -0400

    set up tests with enzyme and jest

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+
+import { shallow } from 'enzyme';
+
+it('renders without crashing', () => {
+
+	const wrapper = shallow(<App />);
+
+	expect(wrapper.exists()).toBe(true);
+});
