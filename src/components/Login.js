a3a9d42c19fbeb19dd582ba9b135825e9d6a18e5
James Stuckey <jstuckey@rutgers.edu>
2019-03-17 01:32:03 -0400

    I forgot to add the NoMatch and Login placeholders to the last commit

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <React.Fragment>
+				<h1>Login form in development...</h1>
+                <form />
+            </React.Fragment>
+        );
+    }
+}
+
+export default Login;
