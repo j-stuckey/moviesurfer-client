26f9c019a97ae3164657d447ad290c76b25062d5
James Stuckey <jstuckey@rutgers.edu>
2019-03-17 18:22:09 -0400

    Added a fieldset and legend to the login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 class Login extends Component {
     render() {
         return (
-            <React.Fragment>
-				<h1>Login form in development...</h1>
-                <form />
-            </React.Fragment>
+            <form>
+                <fieldset>
+                    <legend>Login</legend>
+                </fieldset>
+            </form>
         );
     }
 }
