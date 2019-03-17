b9b3444e9223e4e18fb0f45073dbcb18257e9064
James Stuckey <jstuckey@rutgers.edu>
2019-03-17 01:40:43 -0400

    refactored landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,17 +5,12 @@ import { Redirect } from 'react-router-dom';
 class LandingPage extends Component {
     render() {
         const { isLoggedIn } = this.props;
-        console.log(isLoggedIn);
+
         return (
             <div>
-                {isLoggedIn ? (
-                    <Redirect to="/search" />
-                ) : (
-                    <div>
-                        <h1>Not logged in.</h1>
-                        <h2>Welcome page still in development...</h2>
-                    </div>
-                )}
+                {isLoggedIn && <Redirect to="/search" />}
+                <h1>Not logged in.</h1>
+                <h2>Welcome page still in development...</h2>
             </div>
         );
     }
