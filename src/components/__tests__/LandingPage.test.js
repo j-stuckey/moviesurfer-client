06366b10d907b10e49e492fe41edf0a8b6650204
James Stuckey <jstuckey@rutgers.edu>
2019-03-18 16:11:36 -0400

    Add snapshot with json wrapper
    Renames the file for LandingPage test to LandingPage.test.js.
    Also adds snapshot json serializer for easier snapshot
    comparison.

--- src/components/__tests__/LandingPage.test.js
+++ src/components/__tests__/LandingPage.test.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { LandingPage } from '../LandingPage';
 import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
 
 describe('<LandingPage />', () => {
-    const wrapper = shallow(<LandingPage />);
+	const wrapper = shallow(<LandingPage />);
 
     it('renders without crashing', () => {
         expect(wrapper.exists()).toBe(true);
     });
 
-    it('renders correctly', () => {
-		expect(wrapper).toMatchSnapshot();
-	});
+    it('renders correctly', () => {	
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
 });
