b316b480d031a4c733c477ca70b8d2e992fb5ea3
James Stuckey <jstuckey@rutgers.edu>
2019-03-30 16:47:32 -0400

    Added alt and change logout button

--- src/components/Search.js
+++ src/components/Search.js
@@ -42,7 +42,7 @@ class Search extends React.Component {
                         onChange={this.handleChange}
                     />
                     <div className={styles.semicircle}>
-                        <input type="image" src={glass} className={styles.magnifier}/>
+                        <input type="image" alt="magnifying glass" src={glass} className={styles.magnifier}/>
                     </div>
                 </div>
             </React.Fragment>
