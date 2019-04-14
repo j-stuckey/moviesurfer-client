badf13944a0c9b9ef3940f9bf80f78d5ee478537
James Stuckey <jstuckey@rutgers.edu>
2019-04-13 23:33:21 -0400

    Add PulseLoader component for Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,15 @@ import { login } from '../actions/auth';
 import LoginStyles from './styles/Login.module.css';
 import ButtonStyles from './styles/button.module.css';
 import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
+
+import PulseLoader from 'react-spinners/PulseLoader';
+
+const override = css`
+    display: block;
+    margin: 70px auto;
+    border-color: red;
+`;
 
 export class Login extends Component {
     constructor(props) {
@@ -38,50 +47,61 @@ export class Login extends Component {
         return (
             <React.Fragment>
                 {this.props.isLoggedIn && <Redirect to="/search" />}
-                <form
-                    id="login"
-                    onSubmit={this.handleSubmit}
-                    className={LoginStyles.form}
-                >
-                    <fieldset className={LoginStyles.fieldset}>
-                        <legend className={LoginStyles.title}>Login</legend>
-
-                        {this.props.error && <p>{this.props.error}</p>}
-
-                        <label htmlFor="username" />
-                        <input
-                            className={LoginStyles.inputs}
-                            name="username"
-                            type="text"
-                            placeholder="Username"
-                            value={this.state.username}
-                            onChange={this.handleChange}
-                        />
-
-                        <label htmlFor="password" />
-                        <input
-                            className={LoginStyles.inputs}
-                            name="password"
-                            type="password"
-                            placeholder="Password"
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                        />
-
-                        <button
-                            type="submit"
-                            disabled={!isEnabled}
-                            className={`${LoginStyles.submit} ${
-                                ButtonStyles.registerButton
-                            }`}
+                {this.props.loading ? <PulseLoader
+                    css={override}
+                    sizeUnit={'px'}
+                    size={20}
+                    color={'#51b9f5'}
+                    loading={this.props.loading}
+                /> : 
+                    <React.Fragment>
+                        <form
+                            id="login"
+                            onSubmit={this.handleSubmit}
+                            className={LoginStyles.form}
                         >
+                            <fieldset className={LoginStyles.fieldset}>
+                                <legend className={LoginStyles.title}>Login</legend>
+
+                                {this.props.error && <p>{this.props.error}</p>}
+
+                                <label htmlFor="username" />
+                                <input
+                                    className={LoginStyles.inputs}
+                                    name="username"
+                                    type="text"
+                                    placeholder="Username"
+                                    value={this.state.username}
+                                    onChange={this.handleChange}
+                                />
+
+                                <label htmlFor="password" />
+                                <input
+                                    className={LoginStyles.inputs}
+                                    name="password"
+                                    type="password"
+                                    placeholder="Password"
+                                    value={this.state.password}
+                                    onChange={this.handleChange}
+                                />
+
+                                <button
+                                    type="submit"
+                                    disabled={!isEnabled}
+                                    className={`${LoginStyles.submit} ${
+                                        ButtonStyles.registerButton
+                                    }`}
+                                >
                             Login
-                        </button>
-                    </fieldset>
-                </form>
-                <Link to="/register" className={LoginStyles.registerLink}>
+                                </button>
+                            </fieldset>
+                        </form>
+                        <Link to="/register" className={LoginStyles.registerLink}>
                     Don't have an account? Register here.
-                </Link>
+                        </Link>
+                    </React.Fragment>
+                }
+				
             </React.Fragment>
         );
     }
@@ -96,7 +116,8 @@ Login.propTypes = {
 const mapStateToProps = state => {
     return {
         error: state.auth.error,
-        isLoggedIn: state.auth.currentUser !== null
+        isLoggedIn: state.auth.currentUser !== null,
+        loading: state.auth.loading
     };
 };
 
